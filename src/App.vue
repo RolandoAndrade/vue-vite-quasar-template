65d46079438ea919561ec01036a42ac8f973a0c8
rolandoandrade <rolandoandradefernandez@gmail.com>
2021-11-25 16:33:39 -0400

    Template

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-header>
+      <q-toolbar class="bg-primary">
+        <q-toolbar-title>
+
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+    <q-page-container>
+
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script lang="ts">
+import { ref } from 'vue'
+
+export default {
+  name: 'LayoutDefault',
+
+  components: {
+
+  },
+}
+</script>
+<style>
+
+
+</style>
